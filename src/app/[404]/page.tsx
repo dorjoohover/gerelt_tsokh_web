0a7a601ec39866b6afeff2f02165e8457d844a86
dagina <aaban9494@gmail.com>
2023-11-24 00:29:23 +0800

    port to 80

--- src/app/[404]/page.tsx
+++ src/app/[404]/page.tsx
@@ -1,10 +1,15 @@
 "use client";
+import { Center, Text } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
 
 export default function ErrorPage() {
   const router = useRouter();
   //   if(window !== undefined ) {
-  router.push("/");
+  // router.push("/");
   //   }
-  return <h1>not found</h1>;
+  return (
+    <Center>
+      <Text>Not found</Text>
+    </Center>
+  );
 }
