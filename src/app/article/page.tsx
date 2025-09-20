1d078a05295dc27860e67d21d6e9971739982948
dorjoo <dorjoohover@gmail.com>
2025-09-20 09:12:33 +0800

    a

--- src/app/article/page.tsx
+++ src/app/article/page.tsx
@@ -3,12 +3,33 @@ import { ArticleTypes } from "@/global/enum";
 import { filterName } from "@/global/functions";
 import { articleTags } from "@/values/tags";
 import { ArticlePage } from ".";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Хийсэн ажлууд",
+  description: "Манай хийсэн ажлуудыг үзэх",
+  openGraph: {
+    title: "Хийсэн ажлууд",
+    description: "Манай хийсэн ажлуудыг үзэх",
+    url: "https://tokhiruulga.mn/work?name=advice",
+    siteName: "Tokhiruulga.mn",
+    images: [
+      {
+        url: "http://tokhiruulga.mn/assets/bg.png",
+        width: 1200,
+        height: 630,
+        alt: "Tokhiruulga.mn - Хийсэн ажлууд",
+      },
+    ],
+    locale: "mn_MN",
+    type: "website",
+  },
+};
 const Page = () => {
   return (
     <div>
       {" "}
-      <MetaOg title={"Нийтлэл"} description={"Нийтлэл"} url="/article" />
+      {/* <MetaOg title={"Нийтлэл"} description={"Нийтлэл"} url="/article" /> */}
       <ArticlePage />
     </div>
   );
